85597e2eae5f22726aef2944e989edc45a207f38
Konduru Manikanta <106226301+KonduruManikanta@users.noreply.github.com>
2024-12-13 13:54:12 +0530

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,7 +13,7 @@ const Footer = () => {
   return (
     <div className="footer-bg-container">
       <div>
-        <h2 className="footer-website-name">BriSphere</h2>
+        <h2 className="footer-website-name">MK Travels</h2>
         <p>Spituk, Ladakh,</p>
         <p>India, 194101</p>
         <p>+91 - 7764997033</p>
